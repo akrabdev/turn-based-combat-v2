01acbea2db8e5a7bb9c11b0d029d0d25df001f50
Ak <abdallahakrab98@gmail.com>
2021-05-13 13:49:19 +0200

    ItemSlot Struct
    Information of the item in a slot

---
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// struct uses values not references , we dont need class here
+public struct ItemSlot
+{
+    public InventoryItem item;
+    public int quantity;
+
+    // constructor 
+    public ItemSlot(InventoryItem item, int quantity)
+    {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+}
