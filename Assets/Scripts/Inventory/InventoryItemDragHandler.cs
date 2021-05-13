ec68362a064b9cad1d5615daeae7553f2e70cd1d
Ak <abdallahakrab98@gmail.com>
2021-05-13 20:41:11 +0200

    InventoryItemDragHandler

---
+++ Assets/Scripts/Inventory/InventoryItemDragHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.EventSystems;
+
+public class InventoryItemDragHandler : ItemDragHandler
+{
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+
+            base.OnPointerUp(eventData);
+
+            if (eventData.hovered.Count == 0)
+            {
+                //destroy item or drop item
+            }
+        }
+    }
+
+
+}
