ac665ab0fd489942db1294359978946e94124153
Ak <abdallahakrab98@gmail.com>
2021-05-13 13:52:52 +0200

    ItemContainer Interface

---
+++ Assets/Scripts/Inventory/IItemContainer.cs
@@ -0,0 +1,27 @@
+﻿
+// What an Item Container need?
+public interface IItemContainer
+
+{
+    // in interfaces assumes public by default
+
+    // Add Item Method
+    ItemSlot AddItem(ItemSlot itemSlot);
+
+    // Remove Item Method
+    void RemoveItem(ItemSlot itemSlot);
+    void RemoveAt(int slotIndex);
+
+    // Swap two items in container
+    void Swap(int indexOne, int indexTwo);
+
+    // Container has item?
+    bool HasItem(InventoryItem item);
+
+    // total item quantity in container
+    int GetTotalQuantity(InventoryItem item);
+
+
+
+
+}
