0dca71119dfdcccf66580ec14dcf6368fa0d5497
Ak <abdallahakrab98@gmail.com>
2021-05-13 14:54:59 +0200

    ItemSlot Override == ,!=

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -14,4 +14,8 @@ public struct ItemSlot
         this.quantity = quantity;
     }
 
+    // Override ==: To compare if two slots have same item
+    public static bool operator ==(ItemSlot a, ItemSlot b) { return a.Equals(b); }
+    public static bool operator !=(ItemSlot a, ItemSlot b) { return !a.Equals(b); }
+
 }
