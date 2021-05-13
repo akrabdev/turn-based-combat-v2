99bd90b6626442f83db1c2c850e024c2bf9373c6
Ak <abdallahakrab98@gmail.com>
2021-05-13 20:37:28 +0200

    ItemSlotUI: Base class for Slots of different types

---
+++ Assets/Scripts/Inventory/ItemSlotUI.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+// Base Class for every  slot hotbar slot inventory slot, spell slot
+public abstract class ItemSlotUI : MonoBehaviour, IDropHandler
+{
+    // protected : accessed by children
+    //refernece image
+    [SerializeField] protected Image itemIconImage = null;
+
+    // 
+    public int SlotIndex { get; private set; }
+
+    // GETTER , setter for SlotItem
+    public abstract HotbarItem SlotItem { get; set; }
+    // when opened
+    // Update
+    private void OnEnable() => UpdateSlotUI();
+
+    // first ever  open
+    protected virtual void Start()
+    {
+
+        SlotIndex = transform.GetSiblingIndex();
+        UpdateSlotUI();
+    }
+    // handling dropping item on slot
+    public abstract void OnDrop(PointerEventData eventData);
+    //How Slot is updated , depends on inventory, skills (cooldown etc)
+    public abstract void UpdateSlotUI();
+    // maybe for inventory we need to disable quantity
+    protected virtual void EnableSlotUI(bool enable)
+    {
+        itemIconImage.enabled = enable;
+    }
+
+}
+
