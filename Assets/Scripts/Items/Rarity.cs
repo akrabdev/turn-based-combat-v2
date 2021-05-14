c38b16458a39e4415128608e83f1ee7bc335673b
Ak <abdallahakrab98@gmail.com>
2021-05-14 18:13:35 +0200

    Added Rarity Class

---
+++ Assets/Scripts/Items/Rarity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rarity : MonoBehaviour
+{
+    [SerializeField] private new string name;
+    [SerializeField] private Color textColour;
+
+    public string Name { get { return name; } }
+
+    public Color TextColour { get { return textColour; } }
+
+
+
+}
