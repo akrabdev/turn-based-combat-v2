5e3af4f8167da4eba62ae8342d822a5e80731ea2
Ak <abdallahakrab98@gmail.com>
2021-05-14 15:07:47 +0200

    Implement ItemDestroy

--- Assets/Scripts/Items/Inventories/InventoryItemDragHandler.cs
+++ Assets/Scripts/Items/Inventories/InventoryItemDragHandler.cs
@@ -1,19 +1,28 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class InventoryItemDragHandler : ItemDragHandler
 {
 
+    //Refs:
+    [SerializeField] private ItemDestroyer itemDestroyer = null;
+
     public override void OnPointerUp(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
+            Debug.Log("OnPointerUp");
 
             base.OnPointerUp(eventData);
 
+            Debug.Log(eventData.hovered.Count);
             if (eventData.hovered.Count == 0)
             {
                 //destroy item or drop item
+                InventorySlot thisSlot = ItemSlotUI as InventorySlot;
+                itemDestroyer.Activate(thisSlot.ItemSlot, thisSlot.SlotIndex);
+
+
             }
         }
     }
