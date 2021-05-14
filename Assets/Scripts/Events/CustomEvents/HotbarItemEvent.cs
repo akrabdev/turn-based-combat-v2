e7357a7e261d4538e568b495fc76ea10916e7091
Ak <abdallahakrab98@gmail.com>
2021-05-14 22:03:43 +0200

    HoverItem GameEvent Setup

---
+++ Assets/Scripts/Events/CustomEvents/HotbarItemEvent.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New HotbarItem Event", menuName = "Game Events/HotbarItem Event")]
+public class HotbarItemEvent : BaseGameEvent<HotbarItem> { }
+
---
+++ Assets/Scripts/Events/Listeners/HotbarItemListener.cs
@@ -0,0 +1,4 @@
+﻿
+
+public class HotbarItemListener : BaseGameEventListener<HotbarItem, HotbarItemEvent, UnityHotbarItemEvent> { }
+
