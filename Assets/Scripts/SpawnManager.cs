322ee1bebe359fbf6e3ce09b278f51f0adad4c13
MennaHany58 <mennahany58@yahoo.com>
2021-05-15 23:25:55 +0200

    Updated SpawnManager to be separate from Player

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -32,13 +32,29 @@ public class SpawnManager : MonoBehaviour
     Unit EnemyUnit;
     BattleSystem BS;
     List<GameObject> obj = new List<GameObject>();
+    public static SpawnManager SMInstance;
+
+    void Awake()
+    {
+        if (SMInstance == null)
+        {
+            SMInstance = this;
+        }
+        else if (SMInstance != this)
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+
+
+    }
     //Start is called before the first frame update
     void Start()
     {
-        BS = BattleSystem.instance.GetComponent<BattleSystem>();
-        PlayerUnit = gameObject.GetComponentInParent<Unit>();
+        BS = BattleSystem.instance;
+        PlayerUnit = GameObject.FindGameObjectWithTag("Player").GetComponent<Unit>();
         EnemyUnit = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Unit>();
-        for(int i = 0; i < spanwedObjects.Count; i++)
+        for (int i = 0; i < spanwedObjects.Count; i++)
             spanwedObjects[i].SetActive(false);
         StartCoroutine(ObjectSpawn());
     }
@@ -47,28 +63,33 @@ public class SpawnManager : MonoBehaviour
     {
         while (true)
         {
+            Debug.Log("Spawning");
             //Change time to be more realistic
-            int i = Random.Range(0, spanwedObjects.Count);
-            int j = Random.Range(0, spanwedObjects.Count);
-            int TimeToCreate = Random.Range(6, 9);
-            Vector3 objectSpawn = new Vector3(Random.Range(-8f, 8f), Random.Range(-4f, 4f), -1f);
+            int i = Random.Range(0, spanwedObjects.Count-1);
+            int j = Random.Range(0, spanwedObjects.Count-1);
+            Debug.Log(i);
+            Debug.Log(j);
+            int TimeToCreate = Random.Range(2, 5);
             for (int k = i; k < j; k++)
             {
+                Vector3 objectSpawn = new Vector3(Random.Range(-8f, 8f), Random.Range(-4f, 4f), -1f);
                 obj.Add(Instantiate(spanwedObjects[k], objectSpawn, Quaternion.identity));
-
+                obj[obj.Count-1].SetActive(true);
             }
             for(int k = 0; k < obj.Count; k++)
             {
                 int TimeToDestroy = Random.Range(2, 5);
-                obj[k].SetActive(false);
+                yield return new WaitForSeconds(TimeToDestroy);
+                //obj.Remove(spawn);
                 Destroy(obj[k], TimeToDestroy);
+
             }
             yield return new WaitForSeconds(TimeToCreate);
 
         }
     }
 
-    IEnumerator CollectObject(GameObject obj)
+    public IEnumerator CollectObject(GameObject obj)
     {
         if(obj.CompareTag("Health"))
         {
@@ -96,20 +117,12 @@ public class SpawnManager : MonoBehaviour
             {
                 
             }
-            Destroy(obj);
         }
         else if(obj.CompareTag("XP"))
         {
-            PlayerUnit.addExperience(10);
             Destroy(obj);
+            PlayerUnit.addExperience(10);
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Collision");
-        StartCoroutine(CollectObject(collision.gameObject));
-    }
-
 
 }
