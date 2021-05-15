05a8a2a00c992859d555a82031c5ebfde1c4fbae
Ak <abdallahakrab98@gmail.com>
2021-05-15 19:36:03 +0200

    Hotbar Logic & UI

---
+++ Assets/Scripts/Items/Hotbars/HotbarItemDragHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HotbarItemDragHandler : ItemDragHandler
+{
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+
+            base.OnPointerUp(eventData);
+            //release over nothing
+            if (eventData.hovered.Count == 0)
+            {
+                // clearing the refernce of HotbarSlot the item was in
+                (ItemSlotUI as HotbarSlot).SlotItem = null;
+            }
+        }
+    }
+
+}
