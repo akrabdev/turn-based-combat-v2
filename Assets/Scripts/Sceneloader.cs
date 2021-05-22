b689f30ae7296680ccf51e306dfcb0f04c89c2fa
NouranSherif20 <nouran.sherif98@gmail.com>
2021-05-22 22:21:03 +0200

    main menu 1 and 2 before merge

---
+++ Assets/Scripts/Sceneloader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Sceneloader : MonoBehaviour
+{
+    public void LoadGame(){
+        SceneManager.LoadScene("SampleScene");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
